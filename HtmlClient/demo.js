1acbf17ab8f525ad8cd266c77d127127390d9ab6
DESKTOP-Q1BV30Q\admin003 <admin003>
2025-10-22 02:53:37 -0500

    Update Html,Vue Sample

--- HtmlClient/demo.js
+++ HtmlClient/demo.js
@@ -2,9 +2,9 @@
 // ✒️ api key
 const LICENSE = "sk_navtalk_your_key"
 
-// ✒️ // ✒️ character name. Currently supported characters include: navtalk.Alex, navtalk.Ethan, navtalk.Leo, navtalk.Lily, navtalk.Emma, navtalk.Sophia, navtalk.Mia, navtalk.Chloe, navtalk.Zoe, navtalk.Ava
+// ✒️ // ✒️ character name. Currently supported characters include: navtalk.Ethan, navtalk.Leo, navtalk.Emma, navtalk.Sophia, navtalk.Mia, navtalk.Chloe, navtalk.Zoe, navtalk.Ava
 // You can check the specific images on the official website: https://console.navtalk.ai/login#/playground/realtime_digital_human.
-const CHARACTER_NAME = "navtalk.Alex"
+const CHARACTER_NAME = "navtalk.Zoe"
 
 // ✒️ voice. Currently supported voices include: alloy, ash, ballad, cedar, coral, echo, marin, sage, shimmer, verse
 // You can check the specific voices on the official website: https://console.navtalk.ai/login#/playground/realtime_digital_human.
